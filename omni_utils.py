ff4ce468b72afc0303118b660e8bb2fe4bb905f4
Vincent van Aalten <vincent.aalten@hotmail.com>
2021-03-12 11:26:53 +0100

    FLuid call, Subscription base
    - Added a fluid call function which handles the logic that was initially in Command.execute, which allows a function to be called when its defined signature is uncertain
    - Added the Subscription class, which will be used to subscribe to certain discord objects. Not fully implemented at this stage.

--- omni_utils.py
+++ omni_utils.py
@@ -3,6 +3,29 @@ from inspect import signature, iscoroutinefunction
 # This module provides utility classes that enable simplified communication
 # between the programmer and the Omni interface
 
+async def fluid_call(callable_object, arg_dict):
+    object_signature = signature(callable_object)
+    object_parameters = [param for param in object_signature.parameters]
+
+    provided_params = arg_dict.keys()
+
+    final_args = {}
+    for param in provided_params:
+        if param in object_parameters:
+            final_args[param] = arg_dict[param]
+            object_parameters.remove(param)
+    
+    if object_parameters:
+        raise TypeError("""Error in fluid call:\n Callable {} requires the 
+        following arguments that were not provided:\n{}
+        """.format(callable_object, object_parameters))
+    
+    if iscoroutinefunction(callable_object):
+        return await callable_object(**final_args)
+    else:
+        return callable_object(**final_args)
+
+
 
 class Command():
     """
@@ -16,24 +39,35 @@ class Command():
     
     async def execute(self, arguments, message):
         """
-        Inspects the callable in the instance's function field, calls the function
-        depending on its signature, then returns the result.
+        Does a fluid call on the command function.
         """
+        arg_dict = {
+            'args':arguments,
+            'message':message,
+            'channel':message.channel,
+            'guild':message.guild,
+            'author':message.author
+        }
+        return await fluid_call(self.function, arg_dict)
 
-        func_signature = signature(self.function)
-        params = func_signature.parameters
 
-        arg_dict = {}
+class Subscription():
 
-        if 'args' in params:
-            arg_dict['args'] = arguments
-        if 'message' in params:
-            arg_dict['message'] = message
-
-        if iscoroutinefunction(self.function):
-            return await self.function(**arg_dict)
-        else:
-            return self.function(**arg_dict)
+    def __init__(self, discord_object, create_function = None):
+        if not hasattr(discord_object, 'id'):
+            raise TypeError("""A subscription was created for object:\n
+            {}\n
+            Of type: {}\n
+            This object does not have an id attribute, and hence the subscription 
+            is not valid.""".format(discord_object, type(discord_object)))
+        
+        self.id = discord_object.id
+        self.create_function = create_function
+        self.create()
+    
+    def create(self):
+        if self.create_function:
+            self.create_function(self)
 
 
 class OmniInterface():
@@ -44,6 +78,7 @@ class OmniInterface():
     def __init__(self):
         # Commands being added are stored into the buffer until flush_buffers is called
         self.command_buffer = []
+        self.subscription_buffer = []
         self.module_name = None
 
     def add_command(self, command):
@@ -71,16 +106,32 @@ class OmniInterface():
         # Return the wrapper, which is instantly called
         return add_command_wrapper
     
+    def add_subscription(self, subscription):
+        self.subscription_buffer.append(subscription)
+
+    def subscribe(self, discord_object, create_function = None):
+        sub = Subscription(discord_object, create_function)
+        self.add_subscription(sub)
+        return sub
+
+
     def flush_buffers(self):
         """
-        Currently this function only flushes the command buffer, but it will flush
-        other buffers in the future. It flushes the buffers in such a way that
-        if the methods that filled them have been replaced with the ones defined
-        in omni.py, the buffers will be emptied. If this function is called
-        prematurely however, the buffers stay intact.
+        Flushes the command and subscription buffers. 
+        It flushes the buffers in such a way that if the methods that filled 
+        them have been replaced with the ones defined in omni.py, the buffers 
+        will be emptied. If this function is called prematurely however, 
+        the buffers stay intact.
         """
 
         old_commands = self.command_buffer
         self.command_buffer = []
         for command in old_commands:
             self.add_command(command)
+        
+        old_subscriptions = self.subscription_buffer
+        self.subscription_buffer = []
+        for subscription in old_subscriptions:
+            self.add_subscription(subscription)
+
+
